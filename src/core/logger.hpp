f691c1bf95403e75c72a3097cba8d2f92d0bc797
ToropovAlexei <henschel37rus@gmail.com>
2024-12-19 21:31:03 +0300

    added base logger

--- src/core/logger.hpp
+++ src/core/logger.hpp
@@ -10,48 +10,46 @@ enum class LogLevel { Trace, Debug, Info, Warn, Error, Fatal };
 
 class Logger {
 public:
-  template <typename... Args> static void log(LogLevel level, const std::string &formatStr, Args &&...args) {
-    std::string message = std::format(formatStr, std::forward<Args>(args)...);
-    std::cout << "[" << currentDateTime() << "] ";
-    std::cout << "[" << logLevelToString(level) << "] ";
-    std::cout << message << std::endl;
+  template <LogLevel Level, typename... Args> static void log(std::format_string<Args...> fmt, Args &&...args) {
+    std::cout << "[" << currentTime() << "]";
+    std::cout << "[" << logLevelToString(Level) << "] ";
+    std::cout << std::format(fmt, std::forward<Args>(args)...) << std::endl;
   }
 
 private:
-  static std::string logLevelToString(LogLevel level) {
+  static constexpr std::string logLevelToString(LogLevel level) {
     switch (level) {
     case LogLevel::Trace:
-      return "TRACE";
+      return "\033[37mTRACE\033[0m"; // Grey
     case LogLevel::Debug:
-      return "DEBUG";
+      return "\033[34mDEBUG\033[0m"; // Blue
     case LogLevel::Info:
-      return "INFO";
+      return "\033[32mINFO\033[0m"; // Green
     case LogLevel::Warn:
-      return "WARN";
+      return "\033[33mWARN\033[0m"; // Yellow
     case LogLevel::Error:
-      return "ERROR";
+      return "\033[31mERROR\033[0m"; // Red
     case LogLevel::Fatal:
-      return "FATAL";
+      return "\033[41m\033[37mFATAL\033[0m"; // Red on Grey
     default:
-      return "UNKNOWN";
+      return "\033[0mUNKNOWN\033[0m"; // White
     }
   }
 
-  static std::string currentDateTime() {
+  static std::string currentTime() {
     auto now = std::chrono::system_clock::now();
     auto time = std::chrono::system_clock::to_time_t(now);
     std::tm tm = *std::localtime(&time);
 
     std::stringstream ss;
-    ss << std::put_time(&tm, "%Y-%m-%d %H:%M:%S");
+    ss << std::put_time(&tm, "%H:%M:%S");
     return ss.str();
   }
 };
 
-// Макросы для удобства логирования
-#define LOG_TRACE(fmt, ...) Logger::log(LogLevel::Trace, fmt, __VA_ARGS__)
-#define LOG_DEBUG(fmt, ...) Logger::log(LogLevel::Debug, fmt, __VA_ARGS__)
-#define LOG_INFO(fmt, ...) Logger::log(LogLevel::Info, fmt, __VA_ARGS__)
-#define LOG_WARN(fmt, ...) Logger::log(LogLevel::Warn, fmt, __VA_ARGS__)
-#define LOG_ERROR(fmt, ...) Logger::log(LogLevel::Error, fmt, __VA_ARGS__)
-#define LOG_FATAL(fmt, ...) Logger::log(LogLevel::Fatal, fmt, __VA_ARGS__)
+#define LOG_TRACE(fmt, ...) Logger::log<LogLevel::Trace>(fmt, ##__VA_ARGS__)
+#define LOG_DEBUG(fmt, ...) Logger::log<LogLevel::Debug>(fmt, ##__VA_ARGS__)
+#define LOG_INFO(fmt, ...) Logger::log<LogLevel::Info>(fmt, ##__VA_ARGS__)
+#define LOG_WARN(fmt, ...) Logger::log<LogLevel::Warn>(fmt, ##__VA_ARGS__)
+#define LOG_ERROR(fmt, ...) Logger::log<LogLevel::Error>(fmt, ##__VA_ARGS__)
+#define LOG_FATAL(fmt, ...) Logger::log<LogLevel::Fatal>(fmt, ##__VA_ARGS__)
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "core/logger.hpp"
 
 int main() {
-  LOG_FATAL("Test: {}", 5);
+  LOG_WARN("Test: {}, {}", 1, 2);
+  LOG_ERROR("aadsf");
+  //   std::cout << Log3("Test: {} {}", 1, 2) << std::endl;
   return 0;
 }
