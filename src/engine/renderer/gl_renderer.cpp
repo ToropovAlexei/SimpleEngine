ef6ce1e68c71edc24b1c568e8950c932fbe9858a
ToropovAlexei <henschel37rus@gmail.com>
2025-06-08 15:43:33 +0300

    added debug msges

--- src/engine/renderer/gl_renderer.cpp
+++ src/engine/renderer/gl_renderer.cpp
@@ -1,10 +1,31 @@
 #include "gl_renderer.hpp"
+#include <engine/core/logger.hpp>
 #include <glad/gl.h>
 
 namespace engine::renderer {
+static void GLAPIENTRY debugCallback([[maybe_unused]] GLenum source, [[maybe_unused]] GLenum type,
+                                     [[maybe_unused]] GLuint id, GLenum severity, [[maybe_unused]] GLsizei length,
+                                     const GLchar *message, [[maybe_unused]] const void *userParam) {
+  if (severity == GL_DEBUG_SEVERITY_HIGH) {
+    LOG_ERROR("OpenGL: {}", message);
+  } else if (severity == GL_DEBUG_SEVERITY_MEDIUM) {
+    LOG_WARN("OpenGL: {}", message);
+  } else if (severity == GL_DEBUG_SEVERITY_LOW) {
+    LOG_INFO("OpenGL: {}", message);
+  } else if (severity == GL_DEBUG_SEVERITY_NOTIFICATION) {
+    LOG_TRACE("OpenGL: {}", message);
+  } else {
+    LOG_INFO("OpenGL: {}", message);
+  }
+}
+
 GlRenderer::GlRenderer(SDL_Window *window) : m_window{window} {
   m_glCtx = SDL_GL_CreateContext(m_window);
   gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
+  glEnable(GL_DEBUG_OUTPUT);
+  glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+  glDebugMessageCallback(debugCallback, nullptr);
+  glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
 }
 
 GlRenderer::~GlRenderer() { SDL_GL_DestroyContext(m_glCtx); }
